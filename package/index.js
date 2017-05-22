423ace7757846e87ffe402fd77678289f54f7d9a
estrattonbailey <ericstrattonbailey@gmail.com>
2017-05-22 18:06:13 -0400

    Support 0 duration

--- package/index.js
+++ package/index.js
@@ -6,7 +6,7 @@ export default (target, opts = {}) => {
   if (!target) return
 
   const end = getOffset(target) + (opts.offset || 0)
-  const duration = opts.duration ? opts.duration : opts.speed ? ((end / (opts.speed || 1000)) * 1000) : 500
+  const duration = opts.duration ? opts.duration : opts.speed ? ((end / (opts.speed || 1000)) * 1000) : opts.duration === 0 ? 0 : 500
 
   new Tweezer({
     start: window.pageYOffset,
